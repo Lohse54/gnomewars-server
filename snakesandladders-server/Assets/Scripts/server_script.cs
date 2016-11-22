e9d2152619f64ad1deab4cfe2d70d52c53f44e26
Senpai <lohsea@hotmail.com>
2016-11-22 13:10:24 +0100

    Started on server
    Started

---
+++ snakesandladders-server/Assets/Scripts/server_script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class server_script : MonoBehaviour {
+
+    Thread receiveThread;
+
+    UdpClient listener;
+
+    public static void Main()
+    {
+        int port = 1234;
+
+        
+    }
+
+	void Start () {
+        
+    }
+	
+	void Update () {
+	
+	}
+}
