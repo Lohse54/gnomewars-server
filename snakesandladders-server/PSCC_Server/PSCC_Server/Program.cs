9bc6075cad5413b1d033137e5eb3329a4e0478a5
ellisbellis <egreen15@student.aau.dk>
2016-12-04 17:08:53 +0100

    Class newClient added to server

--- snakesandladders-server/PSCC_Server/PSCC_Server/Program.cs
+++ snakesandladders-server/PSCC_Server/PSCC_Server/Program.cs
@@ -2,11 +2,24 @@
 
 namespace PSCC_Server
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
-}
+
+
+    //Class newClient
+    public class newClient
+    {
+        bool active, active1, yes = true;
+        List<TcpClient> listOfClients = new List<TcpClient>();
+        string[] players = { "Player1", "Player2", "Player3" };
+        int playerSelect = -1, playerNum = 0;
+        int port = 1615;
+        TcpListener listener;
+
+
+        //Listening to new client
+        public newClient()
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            addClients();
+        }
+    }
